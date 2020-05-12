9b0af07667cd548ec2e65e227e2ad7e5f2d425e3
James Ortiz <jamesaortiz75@gmail.com>
2020-05-12 16:21:56 -0400

    Pun Program

---
+++ Fundamentals/pun.c
@@ -0,0 +1,19 @@
+/*
+Author: James Anthony Ortiz
+File: pun.c
+Description: 
+
+
+*/
+
+
+
+
+#include<stdio.h>
+
+int main(void)
+{
+  printf("To C, or not to C, that is the question.\n");
+
+  return 0;
+}
